42fb06567304cfce9e973603837209ff6b9d3797
futantan <luckytantanfu@gmail.com>
2021-11-27 23:07:22 +0800

    add kbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,31 @@
+import {
+  KBarAnimator,
+  KBarPortal,
+  KBarPositioner,
+  KBarProvider,
+  KBarResults,
+  KBarSearch,
+  useMatches,
+} from 'kbar'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+const actions = [
+  {
+    id: 'blog',
+    name: 'Blog',
+    shortcut: ['b'],
+    keywords: 'writing words',
+    perform: () => (window.location.pathname = 'blog'),
+  },
+  {
+    id: 'contact',
+    name: 'Contact',
+    shortcut: ['c'],
+    keywords: 'email',
+    perform: () => (window.location.pathname = 'contact'),
+  },
+]
 const Home: NextPage = () => {
   return (
     <div>
@@ -9,9 +34,40 @@ const Home: NextPage = () => {
         <meta name="description" content="Automatic your workflow" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello, world</main>
+      <KBarProvider actions={actions}>
+        <KBarPortal>
+          <KBarPositioner>
+            <KBarAnimator>
+              <KBarSearch />
+              <RenderResults />
+            </KBarAnimator>
+          </KBarPositioner>
+        </KBarPortal>
+      </KBarProvider>
     </div>
   )
 }
 
+function RenderResults() {
+  const { results } = useMatches()
+
+  return (
+    <KBarResults
+      items={results}
+      onRender={({ item, active }) =>
+        typeof item === 'string' ? (
+          <div>{item}</div>
+        ) : (
+          <div
+            style={{
+              background: active ? '#eee' : 'transparent',
+            }}
+          >
+            {item.name}
+          </div>
+        )
+      }
+    />
+  )
+}
 export default Home
